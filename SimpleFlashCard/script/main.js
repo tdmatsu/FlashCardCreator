bb5125bad8d5bad7deb7741e810a22946274c30a
Tadaaki Matsumoto <tadaaki@gmail.com>
2010-06-10 16:08:38 +0300

    Updated the flash card widget

--- SimpleFlashCard/script/main.js
+++ SimpleFlashCard/script/main.js
@@ -4,11 +4,6 @@
 
 function $(e){return document.getElementById(e);}
 
-// XMLHttpRequest object
-var ajaxRequest;
-
-var flashcards;
-
 ////////////////////////////////////////////////////////////////////////////////
 // Debug utilities
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,40 +40,20 @@ function _log(str_log)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// initOnLoad
+// init
 //----------------------------------------------------------------------------------------------------------------------
-function initOnLoad()
+function init()
 {
 	// WRT specific: hide the softkeys
 	if (navigator.userAgent.indexOf("Series60/5.0") != -1){
 		window.menu.hideSoftkeys();
 	}
-	
+
 	// initialize the start page
 	initStartPage();
 	
-	// initialize the flashcards page
-	flashcards = new FlashCards();
-	flashcards.setInstanceName("flashcards");
-	flashcards.setRegion($("page_flashcards"));
-	
-	// go to the start page
-	gotoStartPage();
-	
-	// set height of each pane
-	setParentSize();
-	$("page_start").style.height = "100%";
-	$("page_flashcards").style.height = "100%";
-	$("page_debug").style.height = "100%";
-	
-	// show/hide debug pane
-	$("page_debug").style.display = DEBUG ? "block" : "none";
-}
-
-function setParentSize()
-{
-	$("parent").style.height = (window.innerHeight - 20) + "px";
-	$("parent").style.width = (window.innerWidth - 20) + "px";
+	// initialize the flash cards
+	initFlashCards();
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -86,43 +61,35 @@ function setParentSize()
 //----------------------------------------------------------------------------------------------------------------------
 function initStartPage()
 {
-	var strwrk = "<SELECT id='selFileName'>";
+	var strwrk = "";
+
 	for (var i = 0; i < FlashCardCollectionNames.length; i++){
-		strwrk = strwrk + "<OPTION value='" + i + "'>" + FlashCardCollectionNames[i] + "<br>";
+		strwrk = strwrk + "<span id='collection_name'><a href='javascript:startFlashCards(" + i + ");'>" + FlashCardCollectionNames[i] + "</a></span><br>";
+		strwrk = strwrk + "<span id='collection_count'>" +  FlashCardCollection[i].length + " entries</span><br><br>";
 	}
-	strwrk += "</SELECT><br>";
-	strwrk += "<input type='button' id='btnStart' value='START!' onclick='javascript:gotoFlashCardPage();'></input><br><br>";
-	strwrk += "Key instructions<br>";
-	strwrk += "1: next<br>";
-	strwrk += "4 or 0: complete the card<br>";
-	strwrk += "9: report<br>";
-	strwrk += "<br>";
-	strwrk += "<input type='checkbox' id='chkAlwaysDone'>Always mark as done</input><br>";
-	strwrk += "<input type='checkbox' id='chkShowControls'>Show Controls</input><br>";
-	strwrk += "<input type='checkbox' id='chkAutoPlay' onclick='javascript:toggleAutoPlay();'>Autoplay</input>";
-	strwrk += "<SELECT id='selSpeed' disabled='true'> ";
-	strwrk += "<OPTION value='2000'>SLOW";
-	strwrk += "<OPTION SELECTED value='1500'>MODERATE";
-	strwrk += "<OPTION value='1000'>FAST";
-	strwrk += "<OPTION value='500'>VERY FAST";
-	strwrk += "</SELECT><br>";
-	
-	$("page_start").innerHTML = strwrk;
+
+	$("section_startpage").innerHTML = strwrk;
 }
 
-function toggleAutoPlay()
+//----------------------------------------------------------------------------------------------------------------------
+// initialize the flash cards
+//----------------------------------------------------------------------------------------------------------------------
+function initFlashCards()
 {
-	if ($("chkAutoPlay").checked == true){
-		$("chkAlwaysDone").checked = true;
-		$("chkAlwaysDone").disabled = true;
-		$("chkShowControls").checked = false;
-		$("chkShowControls").disabled = true;
-		$("selSpeed").disabled = false;
-	} else {
-		$("chkAlwaysDone").disabled = false;
-		$("chkShowControls").disabled = false;
-		$("selSpeed").disabled = true;
-	}
+}
+
+
+function startFlashCards(index)
+{
+	var entries = getFlashCards(index);
+
+	var flashCards = new FlashCards();
+	flashCards.setEntries(entries);
+	flashCards.setRegion($("section_flashcards"));
+	flashCards.startFlashCard();
+	
+	$("section_startpage").style.display = "none";
+	$("section_flashcards").style.display = "block";
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -130,52 +97,17 @@ function toggleAutoPlay()
 //----------------------------------------------------------------------------------------------------------------------
 function gotoStartPage()
 {
-	$("page_start").style.display = "block";
-	$("page_flashcards").style.display = "none";
+	$("section_startpage").style.display = "block";
+	$("section_flashcards").style.display = "none";
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-// gotoFlashCardPage
-//----------------------------------------------------------------------------------------------------------------------
-function gotoFlashCardPage()
-{
-	var entries = getFlashCards($("selFileName").value);
-	flashcards.setEntries(entries);
-	
-	flashcards.setAlwaysDone($("chkAlwaysDone").checked);
-	flashcards.setAutoPlay($("chkAutoPlay").checked, $("selSpeed").value);
-	flashcards.setShowControls($("chkShowControls").checked);
-	flashcards.setExitFunction("gotoStartPage()");
-	flashcards.startFlashCard();
-
-	$("page_start").style.display = "none";
-	$("page_flashcards").style.display = "block";
-}
 
 function getFlashCards(index)
 {
-	 var ret = new Array();
-	 for(var i = 0; i < FlashCardCollection[index].length; i++){
+	var ret = new Array();
+	for(var i = 0; i < FlashCardCollection[index].length; i++){
 	 	ret[i] = FlashCardCollection[index][i];
 	}
 	return ret;
 }
-
-
-//----------------------------------------------------------------------------------------------------------------------
-// keyPress
-//----------------------------------------------------------------------------------------------------------------------
-// Listener
-document.onkeypress = keyPress;
-
-function keyPress(event)
-{
-	flashcards.handleKey(event.charCode);
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-// onResize
-//----------------------------------------------------------------------------------------------------------------------
-//
-window.onresize = setParentSize;
 
